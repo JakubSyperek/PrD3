3b78f1960fc689a53bb1f1a04e98cb0ea8fe3494
JakubSyperek <162420@student.uwm.edu.pl>
2021-03-14 23:06:02 +0100

    delete zad7 and zad8

--- Zad8.py
+++
@@ -1,9 +0,0 @@
-def zakupy(** produkt):
-    for cena in produkt:
-        print("Produkt: ")
-        print(produkt)
-        print("Cena: ")
-        print(cena)
-        print(produkt:cena)
-
-zakupy("chleb":4.00)
